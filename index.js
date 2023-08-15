f2df62aa2d3eb5b965674375e41765a0a3564c1d
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-15 10:01:45 -0300

    finalizando projeto

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ themeToggler.addEventListener('click', () => {
   themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
   themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 });
+
+// preenche tabela de pedidos recentes
+Orders.forEach(order => {
+  const tr = document.createElement('tr');
+  const trContent = `
+                      <td>${order.productName}</td>
+                      <td>${order.productNumber}</td>
+                      <td>${order.paymentStatus}</td>
+                      <td class="${order.shipping === 'Cancelado' ? 'danger' : order.shipping === 'Pendente' ? 'warning' : 'primary'}">${order.shipping}</td>
+                      <td class="primary">Detalhes</td>
+                      `;
+  tr.innerHTML = trContent;
+  document.querySelector('table tbody').appendChild(tr);
+})
--- orders.js
+++ orders.js
@@ -2,6 +2,43 @@ const Orders = [
   {
     productName: 'Samsung Galaxy Z Flip 5 512 GB',
     productNumber: '44756',
-    paymentStatus: ''
-  }
+    paymentStatus: 'A pagar',
+    shipping: 'Pendente'
+  },
+  {
+    productName: 'iPhone 14 Pro Max 128 GB',
+    productNumber: '99642',
+    paymentStatus: 'Aprovado',
+    shipping: 'Enviado'
+  },
+  {
+    productName: 'Motorola Edge 40 256 GB',
+    productNumber: '66821',
+    paymentStatus: 'Reembolso',
+    shipping: 'Cancelado'
+  },
+  {
+    productName: 'Apple Watch Series 8 41mm',
+    productNumber: '99643',
+    paymentStatus: 'Aprovado',
+    shipping: 'Enviado'
+  },
+  {
+    productName: 'Motorola Razr 40 Ultra 256 GB',
+    productNumber: '19123',
+    paymentStatus: 'Aprovado',
+    shipping: 'Enviado'
+  },
+  {
+    productName: 'Samsung Galaxy S23 Ultra 512 GB',
+    productNumber: '44755',
+    paymentStatus: 'A pagar',
+    shipping: 'Pendente'
+  },
+  {
+    productName: 'AirPods Pro 2ª Geração',
+    productNumber: '99647',
+    paymentStatus: 'Rejeitado',
+    shipping: 'Cancelado'
+  },
 ]
